babafc15e403ef65350a1856e40c743fe0c4e273
DUUUUUUUDE <42291216+DUUUUUUUDE@users.noreply.github.com>
2018-10-26 08:54:31 +0200

    1234

---
+++ Portal/Assets/First Person controller/FPS scripts/PlayerScripts/PlayerInput.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    [HideInInspector]
+    public Vector3 _MovementForward;
+
+    Vector3 GetForwardMovement()
+    {
+        float HMovement = Input.GetAxis("Horizontal");
+        float VMovement = Input.GetAxis("Vertical");
+
+        Vector3 moveDirSide = PlayerManager._PlayerManager._playerMovement.transform.right * HMovement;
+        Vector3 moveDirForward = PlayerManager._PlayerManager._playerMovement.transform.forward * VMovement;
+
+        Vector3 dir = new Vector3(moveDirSide.x + moveDirForward.x, 0, moveDirSide.z + moveDirForward.z);
+
+        if (dir.magnitude > 1)
+            return dir.normalized;
+        else
+            return dir;
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+        #region MOVEMENT
+
+        //MOVEMENT VECTOR
+        _MovementForward = GetForwardMovement();
+        //2D MOVEMENT
+        PlayerManager._PlayerManager._playerMovement.SetMoveDirection(_MovementForward);
+
+        // JUMP START
+        if (Input.GetButtonDown("Jump"))
+        {
+            PlayerManager._PlayerManager._playerMovement.StartJump();
+        }
+        // JUMP END
+        if (Input.GetButtonUp("Jump"))
+        {
+            PlayerManager._PlayerManager._playerMovement.EndJump();
+        }
+
+        //RUN START
+        if (Input.GetButtonDown("Fire3"))
+        {
+            PlayerManager._PlayerManager._playerMovement.Run();
+        }
+        //RUN END
+        if (Input.GetButtonUp("Fire3"))
+        {
+            PlayerManager._PlayerManager._playerMovement.Walk();
+        }
+
+        #endregion
+    }
+    
+}
---
+++ Portal/Assets/First Person controller/FPS scripts/PlayerScripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public static PlayerManager _PlayerManager;
+
+
+    public PlayerMovement _playerMovement;
+
+    private void Awake()
+    {
+        if (_PlayerManager == null)
+            _PlayerManager = this;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+            
+    }
+
+    private void Start()
+    {
+        _playerMovement = GetComponent<PlayerMovement>();
+
+    }
+}
